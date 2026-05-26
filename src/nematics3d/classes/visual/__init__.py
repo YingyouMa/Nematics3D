4dc018068c4182d8e25f2a5e1738e8798292fd33
Yingyou Ma <65419573+YingyouMa@users.noreply.github.com>
2026-05-25 21:36:39 -0400

    Add PlotPolyData visual wrapper

--- src/nematics3d/classes/visual/__init__.py
+++ src/nematics3d/classes/visual/__init__.py
@@ -2,6 +2,7 @@
 
 from .plot_contour_surface import OptsContourSurface, PlotContourSurface
 from .plot_delaunay import OptsDelaunay, PlotDelaunay
+from .plot_polydata import OptsPolyData, PlotPolyData
 from .plot_vector import OptsVector, PlotVector
 from .scalar_bar import OptsScalarBar, ScalarBar
 from .scalar_bar_registry import ScalarBarRegistry
@@ -11,6 +12,8 @@ __all__ = [
     "PlotContourSurface",
     "OptsDelaunay",
     "PlotDelaunay",
+    "OptsPolyData",
+    "PlotPolyData",
     "OptsVector",
     "PlotVector",
     "OptsScalarBar",
