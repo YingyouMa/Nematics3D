c65bab8448239a3e646fd60627f8efa443ed4e0b
Yingyou Ma <65419573+YingyouMa@users.noreply.github.com>
2026-08-26 03:19:53 -0400

    Restore as_list as the public list helper

--- src/nematics3d/datatypes/__init__.py
+++ src/nematics3d/datatypes/__init__.py
@@ -7,7 +7,7 @@ from .color_rgb import ColorRGB, as_ColorRGB, as_ColorRGB_array
 from .defect_index import DefectIndex, as_defect_index
 from .dimension_info import DimensionInfo, as_dimension_info
 from .director_field import as_director_field, nField
-from .list import ensure_list
+from .list import as_list
 from .misc import *
 from .number import Number, as_number, as_value_range
 from .q_field import QField5, QField9, as_qfield5, as_qfield9
@@ -15,6 +15,3 @@ from .scalar_field import SField, ScalarField, as_scalar_field
 from .string import as_str
 from .tensor import Tensor, as_tensor
 from .vector import Vect, as_vector
-
-# Temporary compatibility for internal callers not yet renamed.
-as_list = ensure_list
