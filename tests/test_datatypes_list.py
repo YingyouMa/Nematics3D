223e18bfba4a3a767b25dd8a127a4db041727497
Yingyou Ma <65419573+YingyouMa@users.noreply.github.com>
2026-08-26 03:19:45 -0400

    Update list helper tests for as_list

--- tests/test_datatypes_list.py
+++ tests/test_datatypes_list.py
@@ -1,14 +1,14 @@
-from nematics3d.datatypes import ensure_list
+from nematics3d.datatypes import as_list
 
 
 def test_list_is_returned_unchanged():
     value = [1, 2]
-    assert ensure_list(value) is value
+    assert as_list(value) is value
 
 
 def test_tuple_and_set_are_expanded():
-    assert ensure_list((1, 2)) == [1, 2]
-    assert set(ensure_list({1, 2})) == {1, 2}
+    assert as_list((1, 2)) == [1, 2]
+    assert set(as_list({1, 2})) == {1, 2}
 
 
 def test_other_objects_are_treated_as_single_items():
@@ -16,10 +16,10 @@ def test_other_objects_are_treated_as_single_items():
     sequence = range(3)
     generator = (value for value in range(3))
 
-    assert ensure_list(text) == [text]
-    assert ensure_list(sequence) == [sequence]
-    assert ensure_list(generator) == [generator]
+    assert as_list(text) == [text]
+    assert as_list(sequence) == [sequence]
+    assert as_list(generator) == [generator]
 
 
 def test_none_is_a_valid_single_item():
-    assert ensure_list(None) == [None]
+    assert as_list(None) == [None]
