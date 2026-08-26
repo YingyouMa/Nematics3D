04f0c92a7bd763542355c86bcf15f5aecfa33588
Yingyou Ma <65419573+YingyouMa@users.noreply.github.com>
2026-08-26 03:18:11 -0400

    Export ensure_list helper

--- src/nematics3d/datatypes/__init__.py
+++ src/nematics3d/datatypes/__init__.py
@@ -7,6 +7,7 @@ from .color_rgb import ColorRGB, as_ColorRGB, as_ColorRGB_array
 from .defect_index import DefectIndex, as_defect_index
 from .dimension_info import DimensionInfo, as_dimension_info
 from .director_field import as_director_field, nField
+from .list import ensure_list
 from .misc import *
 from .number import Number, as_number, as_value_range
 from .q_field import QField5, QField9, as_qfield5, as_qfield9
