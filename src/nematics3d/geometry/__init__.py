b4c00a1cfc0ae3f821584f736d888de5b458a007
Yingyou Ma <65419573+YingyouMa@users.noreply.github.com>
2026-09-02 02:17:28 -0400

    Export polyline nearest-point helper

--- src/nematics3d/geometry/__init__.py
+++ src/nematics3d/geometry/__init__.py
@@ -2,7 +2,7 @@
 
 from .box import get_box_corners
 from .misc import *
-from .nearest import find_nearest_point
+from .nearest import closest_point_on_polyline, find_nearest_point
 from .polydata import (
     as_polydata_input,
     copy_polydata_geometry,
