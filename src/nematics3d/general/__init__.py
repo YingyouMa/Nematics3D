ead1e4e261e23b783349e70792e140e730599959
Yingyou Ma <65419573+YingyouMa@users.noreply.github.com>
2026-09-02 15:14:26 -0400

    Remove legacy point membership export

--- src/nematics3d/general/__init__.py
+++ src/nematics3d/general/__init__.py
@@ -1,13 +1,11 @@
 """General-purpose helpers awaiting more specific long-term homes."""
 
 from .format import fmt_value
-from .misc import mark_points_membership
 
 __all__ = [
     "closest_point_on_polyline",
     "find_nearest_point",
     "fmt_value",
-    "mark_points_membership",
 ]
 
 
