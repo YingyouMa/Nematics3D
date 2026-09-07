e81e869612cb10a91c9ae5d44bd4471e0aee3ea1
Yingyou Ma <65419573+YingyouMa@users.noreply.github.com>
2026-09-07 17:15:18 -0400

    Update n_color_immerse to selected 0.55 colormap

--- src/nematics3d/field.py
+++ src/nematics3d/field.py
@@ -143,83 +143,58 @@ def n_color_immerse(n: nField) -> List[Tuple]:
     """
     Map a nematic director field to RGB colors for visualization.
 
-    This function encodes the orientation of a unit director vector `n`
-    into RGB color values using a nonlinear polynomial mapping followed by
-    a fixed linear transformation and scaling. The colormap is an immersion
-    from RP^2 to R^3. This ensures that similar orientatioin of n refer to
-    similar colors but different orientations might refer to the same color.
-
-    The colormap is modified from boy's surface.
-
-    The color is specifically desined to be distince on white background,
-    and to get x, y, z direction closed to red, blue and green colors.
-
-    x: [0.90535893, 0.22874911, 0.22062688]
-    y: [0.05416607, 0.27934554, 0.48937438]
-    z: [0.30416607, 0.90434554, 0.22687438]
+    The mapping combines a Boy-surface polynomial immersion of RP^2 with a
+    vividness-optimized affine transform. The selected map maximizes mean
+    OKLab chroma subject to ``J_loc <= 0.55``, calibrated red/green/blue axis
+    tolerances, and the sRGB gamut constraint.
 
     Parameters
     ----------
     n : array_like, shape (..., 3)
         Nematic director field.
-        Can be of arbitrary leading dimensions.
 
     Returns
     -------
-    colors : list of tuples. Each tuple has 3 elements
-        RGB color with values typically in [0, 1], suitable for plotting.
-
-    Examples
-    --------
-    >>> n = np.array([[1, 1, 0], [0, 1, 1], [1, 0, 1]])
-    >>> colors = n_color_immerse(n)
-    >>> colors
-    [(0.39345357, 0.1364875 , 0.60187625),
-     (0.09285714, 0.47428571, 0.605     ),
-     (0.51845357, 0.4489875 , 0.47062625)]
+    colors : list of tuple
+        RGB colors in [0, 1], suitable for plotting.
     """
-
     n = check_Sn(n, "n", is_3d_strict=False, is_norm=True)
 
-    RGB = np.zeros((*(np.shape(n)[:-1]), 3))
-
+    boy = np.empty(n.shape, dtype=float)
     x = n[..., 0]
     y = n[..., 1]
     z = n[..., 2]
-
     x2 = x**2
     y2 = y**2
     z2 = z**2
 
-    RGB[..., 0] = (
-        (2 * x2 - y2 - z2)
-        + 2 * y * z * (y2 - z2)
+    boy[..., 0] = 0.5 * (
+        (2.0 * x2 - y2 - z2)
+        + 2.0 * y * z * (y2 - z2)
         + z * x * (x2 - z2)
         + x * y * (y2 - x2)
     )
-    RGB[..., 1] = (y2 - z2) + z * x * (z2 - x2) + x * y * (y2 - x2)
-    RGB[..., 2] = (x + y + z) * ((x + y + z) ** 3 + 4 * (y - x) * (z - y) * (x - z))
-
-    RGB[..., 0] = RGB[..., 0] / 2
-    RGB[..., 1] = RGB[..., 1] * 7 / 8
-    RGB[..., 2] = RGB[..., 2] / 8
-
-    M = np.array(
-        [
-            [1.01667, -0.3, -0.48333],
-            [-1.01667, -1.5, -1.31667],
-            [-0.18333, 0.3, 1.31667],
-        ]
+    boy[..., 1] = (7.0 / 8.0) * (
+        (y2 - z2) + z * x * (z2 - x2) + x * y * (y2 - x2)
+    )
+    boy[..., 2] = (
+        (1.0 / 8.0)
+        * (x + y + z)
+        * ((x + y + z) ** 3 + 4.0 * (y - x) * (z - y) * (x - z))
     )
 
-    result = np.einsum("...i, ji -> ...j", RGB, M)
+    transform = np.array(
+        [
+            [0.5022508927293965, 0.0814191819777772, 0.4278817282953531],
+            [-0.2622468169155294, 0.4198664552518698, 0.2843783905850694],
+            [-0.2603273418569074, -0.3829942092529955, 0.3705024138608909],
+        ],
+        dtype=float,
+    )
+    offset = np.array(
+        [0.3810134662659256, 0.4051244318995519, 0.4114207201942865],
+        dtype=float,
+    )
 
-    scales = np.array([2.1, 4.2, 2.0])
-    offsets = np.array([0.45, 0.51, 0.23])
-    result = result / scales + offsets
-
-    colors = []
-    for color in result:
-        colors.append(tuple(color))
-
-    return colors
+    result = np.einsum("...i,ji->...j", boy, transform) + offset
+    return [tuple(color) for color in np.clip(result, 0.0, 1.0)]
